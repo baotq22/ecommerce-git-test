0be3216718e5c7bd0f6ec6f323b1c566c0b994cc
baotq22 <tranquocbao2210@gmail.com>
2023-12-14 15:00:03 +0700

    fix bug 1

--- landing-page/index.js
+++ landing-page/index.js
@@ -70,6 +70,14 @@ function displayProducts(perListItem) {
     console.log("items00113", perListItem);
     console.log("items00333", perListItem);
 
+    console.log("items002234", perListItem);
+    console.log("items0023426", perListItem);
+    console.log("items002rtyh", perListItem);
+
+    console.log("items002e5y", perListItem);
+    console.log("items002uy64", perListItem);
+    console.log("items00267455", perListItem);
+
     const baseURL = "../assets/landing-page/"
     // make all item empty
     $("#products__list").empty();
